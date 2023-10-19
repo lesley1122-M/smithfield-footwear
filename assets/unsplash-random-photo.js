0454df9af3ea608ed581972cf5cc34a76eaa7e8f
Lesley Mabaylan <malesleymabaylan@gmail.com>
2023-10-19 19:19:55 +0800

    Unsplash Random Js
    Replace header image randomly using unsplash API

--- assets/unsplash-random-photo.js
+++ assets/unsplash-random-photo.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     const unsplashAccessToken = 'faCtH-n91ioF_1e7k_cSgHheUyGxnGdtvHOQ7TmnEGw';
     const imageElement = $('.image-with-text__media img');
-    
     function getRandomPhoto() {
       $.ajax({
         url: `https://api.unsplash.com/photos/random/?client_id=${unsplashAccessToken}`,
@@ -9,11 +8,9 @@ $(document).ready(function() {
         success: function(data) {
           const photoUrl = data.urls.regular;
   
-          // Hide the original image with a smooth fade-out
           imageElement.fadeOut(100, function() {
-            // Load the new image source and fade it in when it's fully loaded
             imageElement.removeAttr('srcset').attr('src', photoUrl).on('load', function() {
-              imageElement.fadeTo(100, 1); // Fade in to full opacity
+              imageElement.fadeTo(100, 1); 
             });
           });
         },
@@ -22,8 +19,6 @@ $(document).ready(function() {
         }
       });
     }
-  
-    // Call the function to load a random image from Unsplash on page load or refresh
     getRandomPhoto();
   });
   
